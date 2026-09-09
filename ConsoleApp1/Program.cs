54c1e8d588a787e0de6cd8aee73eefca331747bc
Admin <tri_dpm235489@student.agu.edu.vn>
2026-09-09 15:20:25 +0700

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
-using System;
+﻿using System;
 
-namespace DPM235489_PhanMinhTri_RealFactoryPattern_Nhanvienbanhang
+namespace RefactoringGuru.DesignPatterns.FactoryMethod.Conceptual
 {
     // The Creator class declares the factory method that is supposed to return
     // an object of a Product class. The Creator's subclasses usually provide
